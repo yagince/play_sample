659c81020e5f9aff6683436e36c6ed01aec0fc61
Yagi <straitwalk@gmail.com>
2012-04-29 23:24:36 +0900

    テストにコメント追加

--- test/models/UserSpec.scala
+++ test/models/UserSpec.scala
@@ -3,7 +3,11 @@ package models
 import org.specs2.mutable.Specification
 
 class UserSpec extends Specification {
-  "attributes" should {
+  /*
+  * このテストに意味はないが
+  * とりあえず練習だと思って、、、
+  * */
+  " attributes" should {
     val user = User("hoge", "foo", "bar")
     "should has name" in {
       user.name must equalTo("hoge")
