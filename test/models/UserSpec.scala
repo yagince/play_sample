4cf6d8631ec3c2bc399d202dfaec8120c90cbc99
Yagi <straitwalk@gmail.com>
2012-04-29 23:44:17 +0900

    UserのテストをScopeに修正

--- test/models/UserSpec.scala
+++ test/models/UserSpec.scala
@@ -1,6 +1,7 @@
 package models
 
 import org.specs2.mutable.Specification
+import org.specs2.specification._
 
 class UserSpec extends Specification {
   /*
@@ -8,14 +9,18 @@ class UserSpec extends Specification {
   * とりあえず練習だと思って、、、
   * */
   " attributes" should {
-    val user = User("hoge", "foo", "bar")
-    "should has name" in {
+
+    trait sampleUser extends Scope {
+      val user = User("hoge", "foo", "bar")
+    }
+
+    "should has name" in new sampleUser {
       user.name must equalTo("hoge")
     }
-    "should has email" in {
+    "should has email" in new sampleUser {
       user.email must equalTo("foo")
     }
-    "should has password" in {
+    "should has password" in new sampleUser {
       user.password must equalTo("bar")
     }
   }
